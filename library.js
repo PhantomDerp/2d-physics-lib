3aa58ef0e22a05eed12cdd639f276b1d1f54a6c4
phantomderp <phantomderp2005@gmail.com>
2020-04-30 07:15:11 -0400

    b

--- library.js
+++ library.js
@@ -9,6 +9,10 @@ const pi = Math.PI;
 class Obj {
   constructor(shape, mass = 1, xvel = 0, yvel = 0, avel = 0) {
     this.points = shape;
+    //make sure last point is first point
+    if (this.points[0] != this.points[this.points.length - 1]) {
+      this.points.push(this.points[0]);
+    }
     this.mass = mass;
     this.xvel = xvel; //x velocity
     this.yvel = yvel; //y velocity
