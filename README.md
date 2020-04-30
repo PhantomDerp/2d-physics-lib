fef77f56dcd3ff0f70b0517ba27db9ddf86eee9f
phantomderp <phantomderp2005@gmail.com>
2020-04-30 07:05:51 -0400

    a

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 2d physics
 
-First, download the zip into your project's root directory
+1. download the zip
 
-Then, copy and paste into index.html, above the other script tags:  
+2.  copy and paste into index.html, above the other script tags:  
 <script src="2d-physics-lib-master/linear-lib-master/library.js" type="module"></script>
 <script src="2d-physics-lib-master/library.js" type="module"></script>  
 
 The script tags this is used in must also have type="module"  
 
-Finally, in the top of the file where this  is used, add this line:
+3.  in the top of the file where this  is used, add this line:
 import * as physics from './2d-physics-lib-master/library.js';
--- library.js
+++ library.js
@@ -11,10 +11,61 @@ export function getObjFromPoints(points) {
   return new Obj(new linear.Shape(points))
 }
 
-//basic shape object. shape is a linearlib shape
+//basic shape object. shape is an array of [x,y] points
 export class Obj {
-  constructor(shape, mass = 1) {
-    this.shape = shape;
+  constructor(shape, mass = 1, xvel = 0, yvel = 0, avel = 0) {
+    this.points = shape;
     this.mass = mass;
+    this.xvel = xvel; //x velocity
+    this.yvel = yvel; //y velocity
+    this.avel = avel; //angular velocity
+  }
+  //return point where this intersects one object
+  testCollision(obj2) {
+    var shape = linear.Shape()
+  }
+  
+  //return points where multiple objects intersect
+  testCollisions(objs) {
+    
+  }
+  
+  //translate shape along x axis
+  translateX(n) {
+    
+  }
+  
+  //translate shape along y axis
+  translateY(n) {
+    
+  }
+  
+  update() {
+    this.translateX(this.xvel);
+    this.translateY(this.yvel);
   }
 }
+
+
+
+//testing V
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+var object = new Obj([250,200],[300,200],[300,250],[250,250]);
+
+setInterval(function() {
+  
+  
+  
+  //draw
+  ctx.beginPath();
+  ctx.moveTo(object.points[0][0], object.points[0][1]);
+  for (var i = 1; i < object.points; i++) {
+    ctx.lineTo(object.points[i][0], object.points[i][1]);
+  }
+  ctx.stroke();
+  ctx.closePath();
+}, 1000/60)
